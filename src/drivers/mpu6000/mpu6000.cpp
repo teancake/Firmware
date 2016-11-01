4e8413254e614c0b53554a142b8f9e9e287b75e0
teancake <teancake.github@gmail.com>
2016-11-01 17:39:35 +0800

    Commented out the codes for swapping xy axes in the MPU6000 driver.

--- src/drivers/mpu6000/mpu6000.cpp
+++ src/drivers/mpu6000/mpu6000.cpp
@@ -1907,20 +1907,22 @@ MPU6000::measure()
 	/*
 	 * Swap axes and negate y
 	 */
+/*
 	int16_t accel_xt = report.accel_y;
 	int16_t accel_yt = ((report.accel_x == -32768) ? 32767 : -report.accel_x);
 
 	int16_t gyro_xt = report.gyro_y;
 	int16_t gyro_yt = ((report.gyro_x == -32768) ? 32767 : -report.gyro_x);
-
+*/
 	/*
 	 * Apply the swap
 	 */
+/*
 	report.accel_x = accel_xt;
 	report.accel_y = accel_yt;
 	report.gyro_x = gyro_xt;
 	report.gyro_y = gyro_yt;
-
+*/
 	/*
 	 * Report buffers.
 	 */
